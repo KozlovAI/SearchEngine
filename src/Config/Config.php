66ab53df106821fc1751548bbfc52e0bae3c185e
motorway <kapitan-q@ya.ru>
2016-08-25 11:43:08 +0000

    add config class

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -1,29 +1,53 @@
 <?php
 namespace Motorway\SearchEngine\Config;
 
-interface ConfigInterface
+class Config implements ConfigInterface
 {
+	protected $name;
+
+	protected $options;
+
 	/**
 	 * Коснтруктор класса
 	 * 
 	 * @param string|array $options Либо данные, либо путь к файлу
 	 * @param string       $name    имя конфига
 	 */
-	public function __construct($options, $name = false);
+	public function __construct($options, $name = false)
+	{
+		$this->name = $name;
+
+		if (is_array($options)) {
+			$this->options = $options;
+		} else {
+			$this->load($options);
+		}
+	}
 
 	/**
 	 * Загружает конфиг из файла
 	 * 
 	 * @return string путь к файлу
 	 */
-	public function load($path);
+	public function load($path)
+	{
+		if (!file_exists($path)) {
+			throw new \LogicException("Config '{$path}' not found");
+		}
+
+		$this->options = include($path);
+		$this->name = strtolower(basename($path, '.php'));
+	}
 
 	/**
 	 * Возвразает имя конфига
 	 * 
 	 * @return string
 	 */
-	public function getName();
+	public function getName()
+	{
+		return $this->name ?: get_class($this);
+	}
 
 	/**
 	 * Возвращает значение параметра из конфига
@@ -32,5 +56,8 @@ interface ConfigInterface
 	 * @param  mixed  $default значение по умолчанию
 	 * @return mixed
 	 */
-	public function get($name, $default = null);
+	public function get($name, $default = null)
+	{
+		return isset($this->options[$name]) ? $this->options[$name] : $default;
+	}
 }
