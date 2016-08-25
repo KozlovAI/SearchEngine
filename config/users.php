a7ffe3e8fc6302b1ec11f1b913110fcea9394af2
motorway <kapitan-q@ya.ru>
2016-08-25 11:45:05 +0000

    add test php config

---
+++ config/users.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+	'source' => 'mysql://root:root@127.0.0.1/shpinxsearch',
+	
+	'query_pre' => ['SET NAMES utf8'],
+	
+	'query_data' => 'SELECT id, first_name, last_name, parent_name, org_id',
+	
+	'query_post' => [],
+
+	'columns' => [
+		'id'          => 'integer',
+		'first_name'  => 'string',
+		'last_name'   => 'string',
+		'parent_name' => 'string',
+		'org_id'      => 'integer',
+	],
+
+	'listen' => array(
+		'sphinx' => '127.0.0.1:9312',
+		'mysql'  => '127.0.0.1:9306',
+	),
+);
