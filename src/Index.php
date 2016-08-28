59d126ef083d98627172df576906133c2f078269
motorway <kapitan-q@ya.ru>
2016-08-28 11:16:07 +0000

    Добавлена возможность переопределения пути для хранения конфигов

--- src/Index.php
+++ src/Index.php
@@ -1,15 +1,22 @@
 <?php
 namespace Motorway\SearchEngine;
 
+define('SRC_PATH', __DIR__);
+define('ROOT_PATH', realpath(SRC_PATH .'/../'));
+
 class Index
 {
 	protected static $instances = array();
+
+	protected static $configLoadPath = false;
+
+	protected static $configSavePath = false;
 
 	/**
 	 * Возвращает инстанс класс по сохраненному имени
 	 * 
 	 * @param  string  $name
-	 * @param  mixed   $config [description]
+	 * @param  mixed   $config
 	 * @return self
 	 */
 	public static function getInstance($name, $config = false)
@@ -18,11 +25,49 @@ class Index
 			return self::$instances[$name];
 		}
 
-		return self::$instances[$name] = new self($config);
+		return self::$instances[$name] = new self($config ?: $name);
 	}
 
+	/**
+	 * Возвращает или устанавливает директорию по умолчанию для загрузки конфигов
+	 * 
+	 * @param  string $path
+	 * @return string
+	 */
+	public static function configLoadPath($path = false)
+	{
+		if ($path) {
+			return self::$configLoadPath = $path;
+		}
+
+		if (self::$configLoadPath) {
+			return self::$configLoadPath;
+		}
+
+		return ROOT_PATH . '/config/';
+	}
+
+	/**
+	 * Возвращает или устанавливает директорию по умолчанию для выгрузки конфигов
+	 * 
+	 * @param  string $path
+	 * @return string
+	 */
+	public static function configSavePath($path = false)
+	{
+		if ($path) {
+			return self::$configSavePath;
+		}
+
+		if (self::$configSavePath) {
+			return self::$configSavePath;
+		}
+
+		return ROOT_PATH .'/output/';
+	}
+
+
 	protected $config;
-	
 	protected $orm;
 
 	/**
